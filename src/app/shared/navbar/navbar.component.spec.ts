b126a55ac599873ec92e9804b6d7461158e236c4
Peerapat Chanchom <peerapat.chanc@ku.th>
2025-10-01 21:36:08 +0700

    first commit

---
+++ src/app/shared/navbar/navbar.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NavbarComponent } from './navbar.component';
+
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should toggle menu', () => {
+    expect(component.isMenuOpen).toBe(false);
+    component.toggleMenu();
+    expect(component.isMenuOpen).toBe(true);
+    component.toggleMenu();
+    expect(component.isMenuOpen).toBe(false);
+  });
+
+  it('should close menu', () => {
+    component.isMenuOpen = true;
+    component.closeMenu();
+    expect(component.isMenuOpen).toBe(false);
+  });
+});
